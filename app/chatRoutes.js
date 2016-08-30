f0c15157111a4a602bb6c3afad355599d3d36ad2
shubhamqweasd <shubhamqweasd@gmail.com>
2016-08-30 14:28:39 +0530

    chat and style fixes

--- app/chatRoutes.js
+++ app/chatRoutes.js
@@ -5,7 +5,9 @@ module.exports = function(io){
 	io.on('connection', function(socket){
 
 		socket.on('newClient', function(data){
-			allClients.push({socket:socket,client:data})
+			if(alreadyLogged(data)){
+				allClients.push({socket:socket,client:data})
+			}
 			//return current client on connection
 			io.emit('clients',returnClients())
 		});
@@ -32,6 +34,15 @@ module.exports = function(io){
 		return allClients.filter(function(x){
 			return x.socket != socket
 		})
+	}
+
+	function alreadyLogged(newClient){
+		for(var k in allClients){
+			if(allClients[k].client.name == newClient.name){
+				return false
+			}
+		}
+		return true
 	}
 
 }
